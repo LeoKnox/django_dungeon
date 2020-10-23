d97343dbf78835f27926602c7294e30844f01c60
LeoKnox <LeoKnox@users.noreply.github.com>
2020-10-22 19:50:43 -0700

    Add files via upload

--- rooms/models.py
+++ rooms/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
-class Room(models.model):
+class Room(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
+    floor_texture = models.ImageField(upload_to='images/')
     width = models.IntegerField()
     length = models.IntegerField()
