bb484e363e110797a65f744b8cb23062c329e217
LeoKnox <LeoKnox@users.noreply.github.com>
2020-10-22 19:32:59 -0700

    Add files via upload

--- django_dungeon/urls.py
+++ django_dungeon/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import rooms.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/', rooms.views.index, name='index')
 ]
