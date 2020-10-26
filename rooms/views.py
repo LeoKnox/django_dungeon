3408be75ff76744de6c5e89d922aad564444067e
LeoKnox <LeoKnox@users.noreply.github.com>
2020-10-25 20:45:03 -0700

    Add files via upload

--- rooms/views.py
+++ rooms/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rooms.models import Room
+from django.forms.models import model_to_dict
 
 def index(request):
-    room_data = Room.objects.all().values()
-    context = {'rooms':room_data}
-    print(context)
-    return render(request, 'index.html', context)
+    room_data = Room.objects.first()
+    model_to_dict(room_data)
+    print(room_data.name)
+    return render(request, 'index.html', {})
