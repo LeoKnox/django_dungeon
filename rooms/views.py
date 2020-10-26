8476d4baacff8498015e93f10ff20d2a63fdad2c
LeoKnox <LeoKnox@users.noreply.github.com>
2020-10-25 19:54:58 -0700

    Add files via upload

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rooms.models import Room
 
 def index(request):
+    context = Room.objects.all()
     return render(request, 'index.html', {})
