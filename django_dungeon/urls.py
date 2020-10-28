ded12e71d187d923b0aaf35e4a5b876c5523ea0e
LeoKnox <LeoKnox@users.noreply.github.com>
2020-10-27 20:21:20 -0700

    Add files via upload

--- django_dungeon/urls.py
+++ django_dungeon/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('rooms.urls')),
     path('room/', include('rooms.urls')),
+    path('create/', include('rooms.urls')),
 ]
