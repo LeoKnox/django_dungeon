675bb7bf6d6d84488834446bfc42c15170a2c686
LeoKnox <LeoKnox@users.noreply.github.com>
2020-10-27 19:40:26 -0700

    Add files via upload

--- rooms/views.py
+++ rooms/views.py
@@ -4,5 +4,7 @@ from rooms.models import Room
 
 def index(request):
     room_data = Room.objects.values()
-    print(room_data)
     return render(request, 'room/index.html', {'rooms': room_data})
+
+def room(request):
+    return render(request, 'room/room.html', {})
